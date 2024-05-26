75ce297a728f4f601a0566f474ed184d49aa4f83
Luan Lisboa Medeiros <109869151+medeirosluan@users.noreply.github.com>
2024-05-26 14:52:18 -0300

    novo projeto

--- js/script.js
+++ js/script.js
@@ -1,34 +1,26 @@
-const operacao = document.querySelector('#operacao')
-const inPrimeiro = document.querySelector('#inPrimeiro')
-const inSegundo = document.querySelector('#inSegundo')
-const msg = document.querySelector('p')
-const btn = document.querySelector('#btn')
+const button = document.querySelector('button')
 
-function matematica(){
-  resultadoOperacao = ''
+function converteValor(){
+    const valorConverte = document.querySelector('.valor-converte')
+    const valorConvertido = document.querySelector('.valor-convertido')
+    const valorInput = document.querySelector('input').value
+   
+    const valorDolar = 5
+   
+   
+    const valorReal = valorInput / valorDolar
+    
+    valorConverte.innerHTML = new Intl.NumberFormat('pt-BR',{
+        style:'currency',
+        currency:'BRL'
+        
+    }).format(valorInput)
 
-  const numeroUm = parseFloat(inPrimeiro.value)
-  const numeroDois = parseFloat(inSegundo.value)
-  const operacaoSelecionada = operacao.value
-
-
-  switch(operacaoSelecionada){
-    case 'Multiplicação': resultadoOperacao = numeroUm * numeroDois
-    break
-    case 'Divisão': resultadoOperacao = numeroUm / numeroDois
-    break
-    case 'Subtração': resultadoOperacao = numeroUm - numeroDois
-    break
-    case 'Adição' : resultadoOperacao = numeroUm + numeroDois
-    break
- 
-  }
-
-  return resultadoOperacao
-
+    valorConvertido.innerHTML = Intl.NumberFormat('en-US',{
+       style:'currency',
+       currency:'USD' 
+    }).format(valorReal)
+    
 }
 
-btn.addEventListener('click',() =>{
-  const valores = matematica()
-  msg.innerText = `${valores.toFixed(1)}`
-})
+button.addEventListener('click',converteValor)
